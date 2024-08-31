5e7b0722efefafc25d899fa12403f69e47104f43
GyuchanLim <gyuchantech@gmail.com>
2024-08-31 14:04:13 +1200

    add a check for valid numbers
    Calculation is done backwards (multiplying last digit by 3, next by 1 and repeated until the first digit is multiplied by 1. So if the number is not 12 digits long, ISBN13 calculation will not be correct

--- ISBN13_checksum_calculator.rb
+++ ISBN13_checksum_calculator.rb
@@ -3,6 +3,8 @@ class ISBNCheckSum
     total = 0
     count = 3
 
+    raise "#{num} is not 12 digits long" if num / 10**11 == 0
+
     while num != 0
       digit = num % 10
       num = num / 10
--- test_cases_generator.rb
+++ test_cases_generator.rb
@@ -3,11 +3,13 @@ require_relative "ISBN13_checksum_calculator"
 File.open('testcases.txt', 'w') { |file|
   file.write("ISBN13, Checksum\n") 
 
-  (1..100).each do |i|
-    num = rand(10**11..10**12) # Generate a 12 digit number
+  (1..10).each do |i|
+    num = rand(10**10..10**11) # Generate a 12 digit number
     checksum = ISBNCheckSum.calculate(num)
-
+    
     file.write("#{num}, #{checksum}\n") 
+  rescue => e
+    file.write("#{e}\n") 
   end
 }
 
